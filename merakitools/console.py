7e5ae1b1f0ebc372e55647c2729321b33d6b11c9
Billy Zoellers <billy.zoellers@mac.com>
2021-08-27 16:16:57 -0400

    use rich for tracebacks

--- merakitools/console.py
+++ merakitools/console.py
@@ -5,4 +5,7 @@ Billy Zoellers
 CLI tools for managing Meraki networks based on Typer
 """
 from rich.console import Console
+from rich.traceback import install
+
 console = Console()
+install(show_locals=True, console=console)

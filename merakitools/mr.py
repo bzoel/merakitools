8daf9610961feb8d1ddaf1445702829d72ce71aa
Billy Zoellers <billy.zoellers@mac.com>
2021-08-30 10:56:48 -0400

    MR commands list-rf and list-rf-profiles

--- merakitools/mr.py
+++ merakitools/mr.py
@@ -60,6 +60,132 @@ def list_ssid(
   console.print(table)
 
 @app.command()
+def list_rf(
+  organization_name: str,
+  network_name: str
+):
+  """
+  List RF settings for a network
+  """
+  # Get network and confirm it is wireless
+  net = find_network_by_name(organization_name, network_name)
+  if ProductType.wireless not in net["productTypes"]:
+    console.print(f"This network does not contain any MR devices")
+    raise typer.Abort()
+
+  # Maintain a dict of RF profiles
+  rf_profiles = {}
+
+  # Get list of all wireless devices
+  with console.status("Accessing API..."):
+    # Get sorted list of MR devices
+    devices = dashboard.networks.getNetworkDevices(net["id"])
+    devices = [device for device in devices if DeviceModel.MR in device['model']]
+    devices = sorted(devices, key=lambda k: k['name'], reverse=False)
+    
+    # Create a table of MR devices with RF info from API
+    table = table_with_columns(
+      ["RF Profile", "2.4Ghz Manual Settings", "2.4Ghz Actual", "5Ghz Manual Settings", "5Ghz Actual"],
+      title=f"RF Settings for {net['name']}",
+      first_column_name="AP Name"
+    )
+    for device in devices:
+      # Get RF settings and current status
+      device_rf = dashboard.wireless.getDeviceWirelessRadioSettings(serial=device["serial"])
+      device_status = dashboard.wireless.getDeviceWirelessStatus(serial=device["serial"])
+
+      # Get RF profile from stored data or API
+      rf_profile_id = device_rf.get("rfProfileId")
+      if rf_profile_id:
+        if rf_profile_id not in rf_profiles:
+          rf_profiles[rf_profile_id] = dashboard.wireless.getNetworkWirelessRfProfile(net["id"], rf_profile_id)
+
+      # Get first SSID on each band for actual status info
+      try:
+        twoFour_status = next(bss for bss in device_status["basicServiceSets"] if bss["enabled"] and bss["broadcasting"] and bss["band"] == "2.4 GHz")
+      except StopIteration:
+        # No 2.4Ghz enabled
+        twoFour_status = None
+      try:
+        five_status = next(bss for bss in device_status["basicServiceSets"] if bss["enabled"] and bss["broadcasting"] and bss["band"] == "5 GHz")
+      except StopIteration:
+        # No 5Ghz enabled
+        five_status = None
+
+      # Readable string for 2.4GHz settings
+      twoFourGhzSettings = []
+      if device_rf['twoFourGhzSettings']['channel']:
+        twoFourGhzSettings.append(f"ch {device_rf['twoFourGhzSettings']['channel']}")
+      if device_rf['twoFourGhzSettings']['targetPower'] == -1.0:
+        twoFourGhzSettings = ["disabled"]
+      elif device_rf['twoFourGhzSettings']['targetPower']:
+        twoFourGhzSettings.append(f"{device_rf['twoFourGhzSettings']['targetPower']} dBm")
+
+      # Readable string for 5GHz settings
+      fiveGhzSettings = []
+      if device_rf['fiveGhzSettings']['channel']:
+        fiveGhzSettings.append(f"ch {device_rf['fiveGhzSettings']['channel']}")
+      if device_rf['fiveGhzSettings']['targetPower'] == -1.0:
+        fiveGhzSettings = ["disabled"]
+      elif device_rf['fiveGhzSettings']['targetPower']:
+        fiveGhzSettings.append(f"{device_rf['fiveGhzSettings']['targetPower']} dBm")
+
+      table.add_row(
+        device["name"],
+        rf_profiles[rf_profile_id]["name"] if rf_profile_id else "None",
+        " / ".join(twoFourGhzSettings),
+        f"ch {twoFour_status['channel']} / {twoFour_status['power']}" if twoFour_status else "Not broadcasting",
+        " / ".join(fiveGhzSettings),
+        f"ch {five_status['channel']} / {five_status['power']}"  if five_status else "Not broadcasting",
+      )
+  console.print(table)
+
+@app.command()
+def list_rf_profiles(
+  organization_name: str,
+  network_name: str
+):
+  """
+  List RF profiles for a network
+  """
+  # Get network and confirm it is wireless
+  net = find_network_by_name(organization_name, network_name)
+  if ProductType.wireless not in net["productTypes"]:
+    console.print(f"This network does not contain any MR devices")
+    raise typer.Abort()
+
+  # Get RF profiles
+  with console.status("Accessing API..."):
+    rf_profiles = dashboard.wireless.getNetworkWirelessRfProfiles(net["id"], includeTemplateProfiles=True)
+    if not rf_profiles:
+      console.print("No RF Profiles found")
+      raise typer.Abort()
+
+    table = table_with_columns(
+      ["Client Balancing", "Band Settings", "2.4GHz Power", "2.4GHz Min. Bitrate", "2.4GHz AutoChannel", "5GHz Power", "5GHz Min. Bitrate", "5GHz AutoChannel", "5GHz Width"],
+      title=f"RF Profiles for {net['name']}",
+      first_column_name="Name"
+    )
+    
+    for profile in rf_profiles:
+      twoFourGhzChannels = [str(int) for int in profile["twoFourGhzSettings"]["validAutoChannels"]]
+      fiveGhzChannels = [str(int) for int in profile["fiveGhzSettings"]["validAutoChannels"]]
+
+      table.add_row(
+        profile["name"],
+        "[green]Enabled" if profile["clientBalancingEnabled"] else "[red]Disabled",
+        f"{profile['apBandSettings']['bandOperationMode']} / {'[green]Band Steering Enabled' if profile['apBandSettings']['bandSteeringEnabled'] else '[red]Band Steering Disabled'}" if profile['bandSelectionType'] == "ap" else "per SSID",
+        f"{profile['twoFourGhzSettings']['minPower']}-{profile['twoFourGhzSettings']['maxPower']}dBm",
+        f"{profile['twoFourGhzSettings']['minBitrate']}Mbps" if profile['minBitrateType'] == "band" else "per SSID",
+        ", ".join(twoFourGhzChannels),
+        f"{profile['fiveGhzSettings']['minPower']}-{profile['fiveGhzSettings']['maxPower']}dBm",
+        f"{profile['fiveGhzSettings']['minBitrate']}Mbps" if profile['minBitrateType'] == "band" else "per SSID",
+        ", ".join(fiveGhzChannels),
+        profile["fiveGhzSettings"]["channelWidth"] if profile["fiveGhzSettings"]["channelWidth"] == "auto" else f"{profile['fiveGhzSettings']['channelWidth']}MHz",
+        end_section=True)
+    console.print(table)
+
+@app.command()
 def list_mesh(
   organization_name: str,
   network_name: str

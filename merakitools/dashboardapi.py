85c5876d5858b01592937ea51472ed2dcebabf87
Billy Zoellers <billy.zoellers@mac.com>
2025-04-19 10:15:16 -0400

    fail better when no API key defined

--- merakitools/dashboardapi.py
+++ merakitools/dashboardapi.py
@@ -5,9 +5,10 @@ Billy Zoellers
 CLI tools for managing Meraki networks based on Typer
 """
 import meraki
-from meraki.exceptions import APIError
+from meraki.exceptions import APIError, APIKeyError
 from merakitools.__init__ import __version__
 from merakitools.console import console
+import sys
 
 try:
     dashboard = meraki.DashboardAPI(
@@ -16,6 +17,9 @@ try:
         suppress_logging=True,
         caller=f"merakitools/{__version__}",
     )
+except APIKeyError:
+    console.print("[bold][red]No Meraki Dashboard API Key.[/bold]\n\tTry [i]export MERAKI_DASHBOARD_API_KEY=YOUR_KEY_HERE[/i][/red]")
+    sys.exit(1)
 except APIError:
     console.print("[bold red]Unable to connect to the Meraki Dashboard.")
     console.print_exception()
--- merakitools/main.py
+++ merakitools/main.py
@@ -13,12 +13,16 @@ MIN_PYTHON = (3, 9)
 if sys.version_info < MIN_PYTHON:
     sys.exit(f"Python {MIN_PYTHON[0]}.{MIN_PYTHON[1]} or later is required")
 
-app = typer.Typer(rich_markup_mode="rich")
-app.add_typer(orgs.app, name="orgs", help="Meraki organizations")
-app.add_typer(networks.app, name="networks", help="Meraki networks")
-app.add_typer(devices.app, name="devices", help="Meraki devices")
-app.add_typer(mx.app, name="mx", help="Meraki MX appliances")
-app.add_typer(ms.app, name="ms", help="Meraki MS switches")
-app.add_typer(mr.app, name="mr", help="Meraki MR wireless")
-app.add_typer(mt.app, name="mt", help="Meraki MT sensors")
-app.add_typer(msp.app, name="msp", help="Manage multiple networks")
+typer_params = {
+    "no_args_is_help": True,
+}
+
+app = typer.Typer(**typer_params, rich_markup_mode="rich")
+app.add_typer(orgs.app, name="orgs", help="Meraki organizations", **typer_params)
+app.add_typer(networks.app, name="networks", help="Meraki networks", **typer_params)
+app.add_typer(devices.app, name="devices", help="Meraki devices", **typer_params)
+app.add_typer(mx.app, name="mx", help="Meraki MX appliances", **typer_params)
+app.add_typer(ms.app, name="ms", help="Meraki MS switches", **typer_params)
+app.add_typer(mr.app, name="mr", help="Meraki MR wireless", **typer_params)
+app.add_typer(mt.app, name="mt", help="Meraki MT sensors", **typer_params)
+app.add_typer(msp.app, name="msp", help="Manage multiple networks", **typer_params)
